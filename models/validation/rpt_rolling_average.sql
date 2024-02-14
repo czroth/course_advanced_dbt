9dca5aa2fc6398fab21407c5a1635041f6e5af51
Chris Roth <chris@preteckt.com>
2024-02-14 18:34:50 +0000

    Add unit test example.\n\nNote this is different than the mrr from project 3.

--- models/validation/rpt_rolling_average.sql
+++ models/validation/rpt_rolling_average.sql
@@ -1,13 +1,17 @@
+{% set rolling_data = unit_testing_select_table(ref('rolling_test_data'), ref('unit_test_rolling_data')) %}
+-- This calls the macro we just wrote above, and returns our original source table, or our mock dataset,
+-- depending on the value of the unit_testing variable during dbt run
+
 WITH
 
 test_set AS (
     SELECT
         *
     FROM
-        {{ ref('rolling_test_data') }}
+        {{ rolling_data }}
 ),
 
-final AS (
+unrounded AS (
     SELECT
         created_at,
         fruit,
@@ -17,6 +21,18 @@ final AS (
         {{ rolling_average('price', 'fruit', 14) }}
     FROM
         test_set
+),
+
+final AS (
+    SELECT
+        created_at,
+        fruit,
+        price,
+        CAST(avg_3_periods_price AS NUMBER(20, 2)) AS avg_3_periods_price,
+        CAST(avg_7_periods_price AS NUMBER(20, 2)) AS avg_7_periods_price,
+        CAST(avg_14_periods_price AS NUMBER(20, 2)) AS avg_14_periods_price
+    FROM
+        unrounded
 )
 
 SELECT * FROM final
