0781934f2bf4abaaca21a0061ce04e23ec9afa52
Chris Roth <chris@preteckt.com>
2024-02-16 21:28:45 +0000

    Add and implement incremental where macro.

--- models/staging/bingeflix/stg_bingeflix__events.sql
+++ models/staging/bingeflix/stg_bingeflix__events.sql
@@ -9,11 +9,7 @@ WITH source AS (
 
     SELECT * FROM {{ source('bingeflix', 'events') }}
 
-{% if is_incremental() %}
-
-  WHERE created_at > (SELECT DATEADD('day', -3, MAX(created_at)) FROM {{ this }})
-
-{% endif %}
+{{ incremental_where_clause() }}
 
 ),
 
